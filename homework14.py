b410ccaa8a85fa5bd20cd3785e1a4999740ac317
Yurii-Savchuk <51401508+Yurii-Savchuk@users.noreply.github.com>
2020-05-29 00:46:21 +0300

    update homework14
    add task #3

--- homework14.py
+++ homework14.py
@@ -4,8 +4,7 @@
 
 import re
 
-
-num = "AА123s4BB"
+num = "AА1234BB"
 
 def numberplate(num):
     var1 = '[A-ZА-Я]{2}\d{4}[A-ZА-Я]{2}'
@@ -23,3 +22,39 @@ def numberplate(num):
 
 
 numberplate(num)
+
+
+#  Напишите класс, который выбирает из произвольного текста номерные знаки и возвращает их в виде пронумерованного списка
+
+class Numberplate:
+    _text = 'asdasdsadf'
+
+    def __init__(self, text):
+        self._text = text
+
+    def numberplate_parser(self, text):
+        var1 = re.findall('[A-ZА-Я]{2}\d{4}[A-ZА-Я]{2}', text)
+        var2 = re.findall('\d{2}\s\d{3}-\d{2}[A-ZА-Я]{2}', text)
+        var3 = re.findall('[а-яa-zA-ZА-Я]\d{5}[A-ZА-Я]{2}', text)
+        lst = list()
+        n = 0
+
+
+        if var1:
+            n += 1
+            my_str = f'{n} -- {var1}'
+            lst.append(my_str)
+        if var2:
+            n += 1
+            my_str = f'{n} -- {var2}'
+            lst.append(my_str)
+        if var3:
+            n += 1
+            my_str = f'{n} -- {var3}'
+            lst.append(my_str)
+
+        return print(lst)
+
+
+res = Numberplate('asdafafds AА123s4BB aczxcx')
+print(res.numberplate_parser('asdafafds AА1234BB aczxcx AА1234BB a12345BC'))
